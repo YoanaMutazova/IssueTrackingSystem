3932c8abe299b6bc9b9e78cfcac080946cdbf701
YoanaMutazova <yoana9252@gmail.com>
2016-04-20 22:19:48 +0300

    projects

--- app/controllers/projects-controller.js
+++ app/controllers/projects-controller.js
@@ -1,14 +1,15 @@
 angular.module('issueTracker.controllers.projects', [
-        'issueTracker.services.projects'
+        'issueTracker.services.projects',
+        'issueTracker.services.users'
     ])
     .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.when('/projects', {
-                templateUrl: 'app/views/projects.html',
+                templateUrl: 'app/views/projects/projects.html',
                 controller: 'ProjectsController'
             });
 
             $routeProvider.when('/projects/:projectId', {
-                templateUrl: 'app/views/project.html',
+                templateUrl: 'app/views/projects/project.html',
                 controller: 'ProjectsController'
             });
         }
@@ -16,8 +17,9 @@ angular.module('issueTracker.controllers.projects', [
     .controller('ProjectsController', [
         '$scope',
         'projects',
+        'users',
         '$routeParams',
-        function ($scope, projects, $routeParams) {
+        function ($scope, projects, users, $routeParams) {
 
             if (!$routeParams.projectId) {
                 projects.getAllProjects()
@@ -30,6 +32,20 @@ angular.module('issueTracker.controllers.projects', [
                 projects.getProjectById($routeParams.projectId)
                     .then(function (projectInfo) {
                         $scope.projectInfo = projectInfo;
+
+                        users.loggedUser()
+                            .then(function (userInfo) {
+                                var id = userInfo.Id;
+                                if (id === $scope.projectInfo.Lead.Id) {
+                                    $scope.leader = true;
+                                    $scope.template = 'app/views/projects/edit-project.html';
+                                } else {
+                                    $scope.template = 'app/views/projects/project-info.html';
+                                }
+                            }, function (error) {
+                                console.log(error);
+                            });
+
                     }, function (error) {
                         console.log(error);
                     });
@@ -38,6 +54,20 @@ angular.module('issueTracker.controllers.projects', [
                     .then(function (issues) {
                         $scope.issues = issues;
                     }, function (error) {
+                        console.log(error);
+                    });
+            }
+
+            $scope.editProject = function (changedData) {
+                projects.getProjectById($routeParams.projectId)
+                    .then(function (success) {
+                        projects.editProject($routeParams.projectId, success.Lead.Id, changedData)
+                            .then(function (success) {
+                                console.log(success);
+                            }, function (error) {
+                                console.log(error);
+                            });
+                    }, function(error) {
                         console.log(error);
                     });
             }
--- app/services/projects.js
+++ app/services/projects.js
@@ -64,10 +64,37 @@ angular.module('issueTracker.services.projects', [
                 return deferred.promise;
             }
 
+            function editProject(projectId, leadId, projectInfo) {
+                var deferred = $q.defer();
+
+                var token = $cookies.get('access_token');
+
+                var data = {
+                    Name: projectInfo.name,
+                    Description: projectInfo.description,
+                    LeadId: leadId
+                };
+
+                $http({
+                    url: BASE_URL + 'projects/' + projectId,
+                    method: 'PUT',
+                    headers: {'Authorization': 'Bearer ' + token},
+                    data: data
+                })
+                    .then(function (success) {
+                        console.log(success.data);
+                    }, function (error) {
+                        console.log(error);
+                    });
+
+                return deferred.promise;
+            }
+
             return {
                 getAllProjects: getAllProjects,
                 getProjectById: getProjectById,
-                getProjectIssues: getProjectIssues
+                getProjectIssues: getProjectIssues,
+                editProject: editProject
             }
         }
     ]);
